cfe86bdff5113b4871c52652d924272671486567
alessandroTola <alessandro.tola@yahoo.it>
2021-09-08 10:04:28 +0200

    Change the id user type from id to uuid and DB migration file

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,27 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    /**
+     * This variable in true by default.
+     **/
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = Str::uuid();
+        });
+
+    }
     /**
      * The attributes that are mass assignable.
      *

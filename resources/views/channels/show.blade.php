284b8f85e63a97102b5e7f8fccfe5b3b228aabd5
alessandroTola <alessandro.tola@yahoo.it>
2021-09-08 17:19:26 +0200

    Create form to update the resources/views/channels/show.blade.php
    Add image name and description fields
    Possibility to select image from the local directory

---
+++ resources/views/channels/show.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ $channel->name }}</div>
+
+                <div id="update-channel-form" class="card-body">
+                    <form action="{{ route('channels.update', $channel->id) }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        @method('PATCH')
+
+                        <div class="form-group row justify-content-center">
+                            <div class="channel-avatar">
+                                <div onclick="document.getElementById('image').click()" class="channel-avatar-overlay">
+                                    <img src="{{ $channel->image }}" alt="">
+                                </div>
+                            </div>
+                        </div>
+
+                        <input style="display: none" id="image" type="file" name="image">
+
+                        <div class="form-group">
+                            <label for="name" class="form-control-label">
+                                Name
+                            </label>
+                            <input type="text" name="name" id="name" class="form-control" value="{{ $channel->name }}">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="description" class="form-control-label">
+                                Description
+                            </label>
+                            <textarea rows="3" name="description" id="description" class="form-control">
+                                {{ $channel->description }}
+                            </textarea>
+                        </div>
+
+                        <button type="submit" class="btn btn-info">
+                            Update channel
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection

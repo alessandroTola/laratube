de71f9c1e746c45be95e3dfb65b5b82ca9e43e78
alessandroTola <alessandro.tola@yahoo.it>
2021-09-14 16:07:14 +0200

    Create list of video and channels
    Ad search into the home page for searching video and channels
    Update home controller to query the database for the videos and channels

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,10 +14,77 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <form action="">
+                        <input type="text" name="search" class="form-control" placeholder="Search videos and channels">
+                    </form>
                 </div>
             </div>
-        </div>
+
+            @if($channels->count() !== 0)
+            <div class="card mt-5">
+                <div class="card-header">
+                    Channels
+                </div>
+
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <th>Name</th>
+                            <th></th>
+                        </thead>
+                        <tbody>
+                            @foreach($channels as $channel)
+                                <tr>
+                                    <td>
+                                        {{ $channel->name }}
+                                    </td>
+                                    <td>
+                                        <a href="{{ route('channels.show', $channel->id) }}" class="btn btn-sm btn-info">View Channel</a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                    <div class="row justify-content-center">
+                        {{ $channels->appends(request()->query())->links() }}
+                    </div>
+                </div>
+            </div>
+        @endif
+
+        @if($videos->count() !== 0)
+                <div class="card mt-5">
+                    <div class="card-header">
+                        Videos
+                    </div>
+
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <th>Name</th>
+                                <th></th>
+                            </thead>
+                            <tbody>
+                                @foreach($videos as $video)
+                                    <tr>
+                                        <td>
+                                            {{ $video->title }}
+                                        </td>
+                                        <td>
+                                            <a href="{{ route('videos.show', $video->id) }}" class="btn btn-sm btn-info">View Video</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+
+                        <div class="row justify-content-center">
+                            {{ $videos->appends(request()->query())->links() }}
+                        </div>
+                    </div>
+                </div>
+            @endif
     </div>
 </div>
 @endsection
